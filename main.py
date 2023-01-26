bf70f44d10181cb570c810fbe630f8ac1ea0d117
user name <65860449+pshankar1@users.noreply.github.com>
2023-01-26 15:30:36 -0500

    added all new pages and links to them

--- main.py
+++ main.py
@@ -6,9 +6,34 @@ app = flask.Flask(__name__)
 
 #Database Code 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+#https://www.youtube.com/watch?v=HzW8ywijxtQ
 @app.route("/")
 def index():
     return flask.render_template("index.html")
+@app.route('/signUp')
+def signUp():
+    return flask.render_template("signUp.html")
 
-app.run()
+@app.route('/login')
+def login():
+    return flask.render_template("login.html")
+
+@app.route('/home')
+def home():
+    return flask.render_template("home.html")
+
+@app.route('/settings')
+def setting():
+    return flask.render_template("settings.html")
+
+@app.route('/currentConvo')
+def currentConvo():
+    return flask.render_template("currentConvo.html")
+
+@app.route('/createGroup')
+def createGroup():
+    return flask.render_template("createGroup.html")
+
+
+if __name__=='__main__':
+    app.run()
