c4edb8d8749c4e7c8e7c7e27be5c91daef30ab6c
user name <65860449+pshankar1@users.noreply.github.com>
2023-02-14 19:01:42 -0500

    adding google api stuff still need to have button to redirect and connect to comongo

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import os
+import pathlib
+from symbol import flow_stmt
 import flask 
 import db
 from db import User
 import time
 from Group import Group
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import Flow
+
 app = flask.Flask(__name__)
+GOOGLE_CLIENT_ID="722336652195-mej9vpifk9raoivrvo579imk2v58jjhu.apps.googleusercontent.com"
+client_secrets_file=os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+flow=Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile","https://www.googleapis.com/auth/userinfo.email","openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
+    )
 
 #Database Code 
 
@@ -24,7 +36,11 @@ def index():
 def signUp():
     return flask.render_template("signUp.html")
 
-
+@app.route('/signUp')
+def googleSignup():
+    authorization_url,state=flow.authorization_url()
+    session["state"]=state
+    return flask.redirect(authorization_url) 
 @app.route('/signUp',methods=['POST'])
 def trysignUp():
    signup= User().signUp()
