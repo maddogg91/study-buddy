d60d71c9ab7a30469b8716a6d68a6194d3e1bd19
Samuelsiaw <samuelsiaw063@gmail.com>
2023-03-23 23:39:17 -0400

    updated requirements

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ from threading import Lock
 import json
 
 
-thread= None
-thread_lock= Lock()
-#returns list of all users
-active_users= db.get_users()
+THREAD = None
+thread_lock = Lock()
+# returns list of all users
+active_users = db.get_users()
 
 
 SCOPES = ['https://www.googleapis.com/auth/calendar',
@@ -29,23 +29,23 @@ SCOPES = ['https://www.googleapis.com/auth/calendar',
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-
- # App configuration
+# App configuration
 def create_app():
 
     app = Flask(__name__)
-    app.config['UPLOAD_FOLDER']= basedir+ "/static/uploads"
-    app.config['MAX_CONTENT_PATH']= 150000
+    app.config['UPLOAD_FOLDER'] = basedir + "/static/uploads"
+    app.config['MAX_CONTENT_PATH'] = 150000
     app.config['SERVER_NAME'] = "127.0.0.1:5000"
-    app.config['DEBUG']= True
+    app.config['DEBUG'] = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.secret_key = os.environ.get(
         "FLASK_SECRET_KEY", default="supersecretkey")
 
     return app
 
-app= create_app()
-socketio= SocketIO(app, cors_allowed_origins='*')
+
+app = create_app()
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 """
 Stream messages as they come in 
@@ -54,8 +54,8 @@ Stream messages as they come in
 
 def get_user_messages():
     """request messages from user"""
-    messages= []
-    groups= session.get("groups")
+    messages = []
+    groups = session.get("groups")
     for i in groups:
         messages.append(db.loadGroupMessages(i))
     return messages
@@ -79,11 +79,12 @@ with open('keys/s.txt', 'rb') as p:
 
 SECR = enc.decrypt(s)
 
-client_secrets_file=os.path.join(pathlib.Path(__file__).parent,"client_secret.json")
+client_secrets_file = os.path.join(
+    pathlib.Path(__file__).parent, "client_secret.json")
 
-session= {
-"start": False,
-"user": ""
+session = {
+    "start": False,
+    "user": ""
 }
 
 
@@ -110,6 +111,7 @@ def disconnect():
     """Disconnecting socket"""
     print("disconnected")
 
+
 @app.route("/google-login")
 def google_login():
     """loggin in to google"""
@@ -124,7 +126,7 @@ def google_callback():
     """ calling back"""
     code = request.args.get("code")
     token_url = "https://oauth2.googleapis.com/token"
-    session["start"]= True
+    session["start"] = True
 
     data = {
         "code": code,
@@ -150,29 +152,28 @@ def google_callback():
         return redirect(url_for("/home"))
 
 
-
 @app.route('/signUp')
 def signUp():
-    """routing to to signup""" 
+    """routing to to signup"""
     return render_template("signUp.html")
 
 
-@app.route('/signUp',methods=['POST'])
-
+@app.route('/signUp', methods=['POST'])
 def trysignUp():
-    """routing to to signup"""  
-    active_usernames=[]
+    """routing to to signup"""
+    active_usernames = []
     for user in active_users:
         active_usernames.append(user["username"])
-    if(request.form["name"] in active_usernames):
-        return render_template("signUp.html", alarm="1") 
+    if (request.form["name"] in active_usernames):
+        return render_template("signUp.html", alarm="1")
     signup = User().signUp()
     if signup == True:
         session["user"] = signup
         return render_template("home.html")
     else:
-        return render_template("signUp.html", alarm="1") 
-    
+        return render_template("signUp.html", alarm="1")
+
+
 @app.route('/changeInfo', methods=['POST'])
 def changeInfo():
     """Changing user info"""
@@ -203,6 +204,8 @@ def login():
     return redirect('/home')
 
 # Logs out current session
+
+
 @app.route('/logout')
 def logout():
     """routing to logout"""
@@ -210,6 +213,7 @@ def logout():
         return render_template("login.html")
     session['user'] = ""
     return redirect('/')
+
 
 @app.route('/login', methods=["POST"])
 def trylogin():
@@ -234,18 +238,19 @@ def home():
     if not session.get("user") and not session.get("email"):
         return redirect('/')
     if not session.get("groups"):
-        groups= []
-    #Searches db for groups by user id 
-    userchats= db.userChats(session.get("user").get("_id"))
-    #Need a route to send to a page without userchats for chats under 1
+        groups = []
+    # Searches db for groups by user id
+    userchats = db.userChats(session.get("user").get("_id"))
+    # Need a route to send to a page without userchats for chats under 1
     if len(userchats) > 0:
         for userchat in userchats:
-            groups.append(userchat["_id"])    
-    session["groups"]= groups
+            groups.append(userchat["_id"])
+    session["groups"] = groups
     if session.get("user").get("username"):
         return render_template("home.html", user=session.get("user").get("username"))
     if session.get("user").get("email"):
-        return render_template("home.html", user= session.get("user").get("email"))
+        return render_template("home.html", user=session.get("user").get("email"))
+
 
 @app.route('/search')
 def search():
@@ -256,46 +261,47 @@ def search():
 @app.route('/search1', methods=["GET"])
 def searchDB():
     """routing to searchDB"""
-    query= request.args.get('query')
-    key= ""
-    query= query.split(": ")
+    query = request.args.get('query')
+    key = ""
+    query = query.split(": ")
     if len(query) < 2:
         results = db.existingChats(query[0], "name")
     else:
         flter = query[0].strip()
         keyword = query[1]
-        chats= []
-        #searches DB by username
-        if flter== "user":
-            results, profiles= db.searchUsers(keyword, "username")
-            key= "user"
-        #searches DB by group name    
-        elif flter== "group":
-            results= db.existingChats(keyword, "name")
-            key= "group"
-        #searches DB by group description    
-        elif flter== "gdesc":
-            results= db.existingChats(keyword, "description")
-            key= "group"
-        #search DB by user messages coming soon    
+        chats = []
+        # searches DB by username
+        if flter == "user":
+            results, profiles = db.searchUsers(keyword, "username")
+            key = "user"
+        # searches DB by group name
+        elif flter == "group":
+            results = db.existingChats(keyword, "name")
+            key = "group"
+        # searches DB by group description
+        elif flter == "gdesc":
+            results = db.existingChats(keyword, "description")
+            key = "group"
+        # search DB by user messages coming soon
     if results != "No results found...":
-        if key== "user":
-            res= {
-                "results" : results,
+        if key == "user":
+            res = {
+                "results": results,
                 "profiles": profiles
             }
-            return render_template("results.html", groups=[], results= res, key= key)
+            return render_template("results.html", groups=[], results=res, key=key)
         else:
             return render_template("results.html",
-            results= results, groups= session.get("groups"), key= key)
-    else: 
-        return render_template("results.html", groups=[], results= [], key= key)
-        
+                                   results=results, groups=session.get("groups"), key=key)
+    else:
+        return render_template("results.html", groups=[], results=[], key=key)
+
+
 @app.route('/settings')
 def setting():
     """routing to SETTINGS"""
     if not session.get("type"):
-     #To regular settings
+     # To regular settings
         return render_template('settings.html')
     else:
         return render_template('googleSettings.html')
@@ -306,7 +312,7 @@ def quiz():
     """routing to quiz"""
     if not session.get("user"):
         return redirect('/')
-    mm= db.loadQuizAnswers(session.get("user").get("_id"))
+    mm = db.loadQuizAnswers(session.get("user").get("_id"))
     print(mm)
     return render_template("quiz.html", mm=mm)
 
@@ -314,19 +320,20 @@ def quiz():
 @app.route('/savequiz', methods=["POST"])
 def savequiz():
     """routing to saved quiz after receiving them"""
-    data= request.form
+    data = request.form
     db.savequiz(data, session.get("user").get("_id"))
     return redirect('quiz')
 
-@app.route('/createGroup', methods= ["GET", "POST"])
+
+@app.route('/createGroup', methods=["GET", "POST"])
 def createGroup():
     """routing to createGroup"""
 
     if not session.get("user"):
         return redirect('/')
     if request.method == "POST":
-        userid= session.get("user").get("_id")
-        photo= request.files['groupPhoto']
+        userid = session.get("user").get("_id")
+        photo = request.files['groupPhoto']
         upload(photo)
         db.createChat(request, photo, userid)
         return redirect('/existingGroups')
@@ -335,10 +342,11 @@ def createGroup():
 
 def upload(file):
     """routing to file uploaded"""
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
+    file.save(os.path.join(
+        app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
 
 
-@app.route('/existingGroups', methods= ["GET", "POST"])
+@app.route('/existingGroups', methods=["GET", "POST"])
 def currentGroups():
     """routing to currentGroups"""
     if not session.get("user"):
@@ -346,20 +354,21 @@ def currentGroups():
     messages = []
     groups = []
     # Searches db for groups by user id
-    userchats= db.userChats(session.get("user").get("_id"))
-    #Need a route to send to a page without userchats for chats under 1
+    userchats = db.userChats(session.get("user").get("_id"))
+    # Need a route to send to a page without userchats for chats under 1
     if len(userchats) > 0:
         for userchat in userchats:
             groups.append(userchat["_id"])
             messages.append(db.loadGroupMessages(userchat["_id"]))
-        session["groups"]= groups
+        session["groups"] = groups
         return render_template("existingGroups.html",
                                user=session.get("user"),
                                len=len(userchats),
                                results=userchats, messages=messages)
     else:
-        #Temporary, sending to create group or would it be better to send to search page???
+        # Temporary, sending to create group or would it be better to send to search page???
         return redirect("/createGroup")
+
 
 @socketio.on('savemessage')
 def saveUserMessage(json):
@@ -374,26 +383,28 @@ def userProfile():
     if not session.get("user"):
         return redirect('/')
     if request.method == "POST":
-        photo= request.files['profilepic']
+        photo = request.files['profilepic']
         print(photo.filename)
         if photo.filename == "" or None:
             print("No photo added")
         else:
-            #uploads a photo if given
+            # uploads a photo if given
             upload(photo)
         db.saveUserProfile(session.get("user").get("_id"), request)
-    profile= db.userProfile(session.get("user").get("_id"))
-    return render_template("profile.html", profile= profile)
-     
-@app.route('/join', methods = ["POST"])
+    profile = db.userProfile(session.get("user").get("_id"))
+    return render_template("profile.html", profile=profile)
+
+
+@app.route('/join', methods=["POST"])
 def joingroup():
     if request.method == "POST":
-        value= request.form['join']
+        value = request.form['join']
         db.joingroup(value, session.get("user").get("_id"))
         return redirect('/existingGroups')
-        
-#created a reloader for easier code running in localhost
-#debug to find bugs
-if __name__=='__main__':
-    #Added websocket functionality to stream data while running.
+
+
+# created a reloader for easier code running in localhost
+# debug to find bugs
+if __name__ == '__main__':
+    # Added websocket functionality to stream data while running.
     socketio.run(app)
