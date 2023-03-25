c5190ea079afed40a26a4a74cd5b4e6a415f36d4
Rob Charity <40213091+maddogg91@users.noreply.github.com>
2023-03-24 22:14:47 -0400

    Update test_try_login.py

--- test/test_try_login.py
+++ test/test_try_login.py
@@ -3,15 +3,6 @@ import sys
 import os
 import requests
 
-
-directory = os.path.dirname(os.path.realpath(__file__))
-
-parent = os.path.dirname(directory)
-
-sys.path.append(parent)
-
-from main import create_app
-
 def test_index():
     response= requests.get('http://127.0.0.1:5000')
     assert response.status_code == 200
