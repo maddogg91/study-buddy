743a34ed679d9bd439c3f0be8f86764aea21c61f
maddogg91 <40213091+maddogg91@users.noreply.github.com>
2023-04-10 11:34:48 -0400

    temporary message addition to existing chat

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def create_app():
     app = Flask(__name__)
     app.config['UPLOAD_FOLDER'] = basedir + "/static/uploads"
     app.config['MAX_CONTENT_PATH'] = 150000
-    app.config['SERVER_NAME'] = "studybuddy.fly.dev"
+    app.config['SERVER_NAME'] = "127.0.0.1:5000"
     app.config['DEBUG'] = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.secret_key = os.environ.get(
